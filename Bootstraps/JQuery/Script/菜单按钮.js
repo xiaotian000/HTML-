fde3e0da69a9d5bb30b20af14ee0cc5b61b03a9e
xiaotian000 <1756768668@qq.com>
2016-07-30 11:01:02 +0800

    菜单

--- Bootstraps/JQuery/Script/菜单按钮.js
+++ Bootstraps/JQuery/Script/菜单按钮.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
        $f = $(".fish").parent().parent().detach();
     });
     $(".hamburger").replaceWith("<li class='portobello'><em>Portobello</em></li>");
+    $(".portobello").parent().parent().addClass("veg_leaf");
     $(".meat").after("<li class='tofu'><em>Tofu</em></li>");
     $m = $(".meat").detach();
+    $(".tofu").parent().parent().addClass("veg_leaf");
     $("button#restoreMe").click(function(){
         $(".menu_entrees li").first().before($f);
+        $(".tofu").parent().parent().addClass("veg_leaf");
         $(".portobello").replaceWith("<li class='hamburger'><em>hamburger</em></li>");
         $(".tufo").each(function(i){
             $(this).after($m[i]);
         });
         $(".tofu").remove();
     });
+    $(".tofu").parent().parent().addClass("veg_leaf");
 });
