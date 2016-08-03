623a3276da786221a38a64d616cf2dfa6f1ac003
xiaotian000 <1756768668@qq.com>
2016-08-03 15:02:43 +0800

    window

--- Bootstraps/JQuery/Script/monster_styles.js
+++ Bootstraps/JQuery/Script/monster_styles.js
@@ -56,7 +56,35 @@ var headclix = 0, eyeclix = 0, noseclix = 0, mouthclix = 0;
 		}
 	
 	});
-	
+	golightning();
+window.onblur=stoplightning;
+window.onfocus=golightning;
+var int1,int2,int3;
+function stoplightning(){
+   window.clearInterval(int1);
+   window.clearInterval(int2);
+   window.clearInterval(int3);
+}
+function lightning_one(){
+    $("#container #lightning1").fadeIn(250).fadeOut(250);
+};
+function lightning_two(){
+    $("#container #lightning2").fadeIn(250).fadeOut(250);
+};
+function lightning_three(){
+    $("#container #lightning3").fadeIn(250).fadeOut(250);
+};
+function golightning(){
+   int1=setInterval(function(){
+       lightning_one();
+   },4000);
+   int2=setInterval(function(){
+       lightning_two();
+   },5000);
+   int3=setInterval(function(){
+       lightning_three();
+   },7000);
+};
 
 
 });//end doc.onready function
